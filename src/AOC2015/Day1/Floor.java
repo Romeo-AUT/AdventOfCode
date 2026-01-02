b78bb805f8df43b355c6eedc80e6ba6e3af89ffe
romeo <romeo.pelzelmayer@icloud.com>
2026-01-02 16:09:38 +0100

    Part 1

---
+++ src/AOC2015/Day1/Floor.java
@@ -0,0 +1,35 @@
+package src.AOC2015.Day1;
+
+import java.io.*;
+
+public class Floor{
+    public static void main(String[] args){
+        Floor f = new Floor();
+        System.out.println(f.getFloor(f.getZeichen()));
+    }
+
+    public String getZeichen(){
+        String path = "input.txt";
+        String temp = "";
+        try(BufferedReader reader = new BufferedReader(new FileReader(path))){
+            String line;
+            while ((line = reader.readLine()) != null){
+                temp += line;
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return temp;
+    }
+
+    public int getFloor(String zeichen){
+        int floor = 0;
+        for(int i = 0; i < zeichen.length(); i++){
+            if(zeichen.charAt(i) == '(')
+                floor++;
+            else
+                floor --;
+        }
+        return floor;
+    }
+}
